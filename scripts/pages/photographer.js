2a3e48a0bb0e8603c926a73ee9e8a05af71fc9e8
maqsse <maqsse@yahoo.fr>
2022-09-30 15:09:14 +0200

    filtre

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -105,8 +105,99 @@ async function updateTotalLikes() {
     const {  media } = await getMedia();
          displayData(media);
          updateTotalLikes();
-         
+         orderWork();
          
 }
 init();
 
+//filtre
+function dropdown(event) {
+    const button = event.currentTarget;
+    const dropdown = button.parentNode;
+    dropdown.classList.toggle('dropdown-open');
+    if (dropdown.classList.contains('dropdown-open')) {
+        button.setAttribute('aria-expanded', true);
+    } else {
+        button.setAttribute('aria-expanded', false);
+    }
+    
+    setTimeout( () => button.focus() , 50);
+}
+
+function selectDropdownOption(event) {
+    const target = event.currentTarget;
+    const option = target.dataset.value;
+    const dropdownList = target.parentNode;
+    const dropdown = target.parentNode.parentNode;
+    const button = dropdown.querySelector("button");
+
+    const currentDropdown = dropdown.querySelectorAll(".dropdown-hide");
+    for (let i = 0; i < currentDropdown.length; i++) {
+        currentDropdown[i].classList.remove("dropdown-hide");
+        currentDropdown[i].setAttribute("aria-selected","false");
+    }
+    
+    target.classList.add("dropdown-hide");
+    target.setAttribute("aria-selected","true");
+
+    dropdown.dataset.value = option;
+    dropdown.querySelector('button').textContent = dropdown.querySelector(`[data-value=${option}]`).textContent;
+
+    dropdown.classList.toggle('dropdown-open');
+    if (dropdown.classList.contains('dropdown-open')) {
+        button.setAttribute('aria-expanded', true);
+    } else {
+        button.setAttribute('aria-expanded', false);
+    }
+    dropdownList.setAttribute("aria-activedescendant",target.id);
+
+    orderWork();
+    setTimeout( () => dropdown.querySelector('button').focus() , 50);
+    
+}
+
+function orderWork() {
+    const photographWork = document.querySelector(".photograph-work");
+    let contentNodes = document.querySelectorAll('.thumb-imgfull');
+    const order = document.querySelector(".dropdown").dataset.value;
+    // Converti la nodelist en array, le call appelle la nodelist en tant que 'this' dans la méthode et array.prototype défini le type de 'this'
+    let content = Array.prototype.slice.call(contentNodes);
+    
+    switch (order) {
+        case "popularity":
+            // De + à -
+            content.sort(
+                function(item, nextItem){
+                    let firstNumber = parseInt(item.querySelector(".number-likes").textContent);
+                    let secondNumber = parseInt(nextItem.querySelector(".number-likes").textContent);
+                    return secondNumber - firstNumber;
+                }
+            )
+            break;
+        case "date":
+            // De + à -
+            content.sort(
+                function(item, nextItem){
+                    let firstString = item.querySelector("[data-date]").dataset.date;
+                    let secondString = nextItem.querySelector("[data-date]").dataset.date;
+                    return secondString.localeCompare(firstString);
+                }
+            )
+            break;
+        case "title":
+            // De A à B
+            content.sort(
+                function(item, nextItem){
+                    let firstString = item.querySelector(".thumb-imgfull>:nth-child(2)").textContent.toLowerCase();
+                    let secondString = nextItem.querySelector(".thumb-imgfull>:nth-child(2)").textContent.toLowerCase();
+                    return firstString.localeCompare(secondString);
+                }
+            )
+            break;
+        default:
+            break;
+    }
+
+    photographWork.innerHTML = "";
+    content.forEach(item => photographWork.appendChild(item));
+}
